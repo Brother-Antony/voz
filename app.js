a0aa89a78e59bb539bad4ff14cedcf57e97f3fdb
Brother-Antony <65048311+Brother-Antony@users.noreply.github.com>
2020-09-21 12:01:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+let rec;
+    if (!("webkitSpeechRecognition" in window)) {
+       alert("disculpas, no puedes usar la API");
+    } else {
+       rec = new webkitSpeechRecognition();
+       rec.lang = "es-PE";
+       rec.continuous = true;
+       rec.interim = true;
+       rec.addEventListener(" result", iniciar);
+    }
+function iniciar(event) {
+    for (i = event.resultIndex; i < event.results.length; i++) {
+       document.getElementById('texto').innerHTML = event.results[i][0].transcript;
+    }
+}
 
+rec.start();
